a7943c4b4d37d01cb624b50a102f4d6f373f9768
Tom Xu <txu@guidehouse.com>
2022-04-18 21:18:17 -0400

    bst improves

--- python_data_structure/python-binary-tree/tree_order.py
+++ python_data_structure/python-binary-tree/tree_order.py
@@ -30,7 +30,20 @@ def post_order_traversal(root=None):
   right = post_order_traversal(root.right)
   return [*left] + [*right] + [root.value]
 
-
+def pre_order_traversal(root=None):
+  """
+  1. Define pre_order_traversal function.
+  2. If the root is null, return an empty list.
+  3. Get the list of values for visiting the left node.
+  4. Get the list of values for visiting the right node.
+  5. Return the root value, concatenated with the left list values
+     concatenated with the right list values.
+  """
+  if root == None:
+    return []
+  left = pre_order_traversal(root.left)
+  right = pre_order_traversal(root.right)
+  return [root.value] + [*left] + [*right]
 
 root = TreeNode('a')
 b = TreeNode('b')
@@ -45,7 +58,9 @@ b.left = d
 b.right = e
 c.right = f
 
-# print(in_order_traversal())         # []
-#print(in_order_traversal(root))     # ['d', 'b', 'e', 'a', 'c', 'f']
-print(post_order_traversal())       # []
-print(post_order_traversal(root))   # ['d', 'e', 'b', 'f', 'c', 'a']
+print(in_order_traversal())        # []
+print(in_order_traversal(root))    # ['d', 'b', 'e', 'a', 'c', 'f']
+print(post_order_traversal())      # []
+print(post_order_traversal(root))  # ['d', 'e', 'b', 'f', 'c', 'a']
+print(pre_order_traversal())       # []
+print(pre_order_traversal(root))   # ['a', 'b', 'd', 'e', 'c', 'f']
--- python_data_structure/python-bst/bst.py
+++ python_data_structure/python-bst/bst.py
@@ -1,96 +1,124 @@
 from tree_node import TreeNode
 
 class BinarySearchTree:
-  def __init__(self, value):
-    self._root = None
-    self._value = value
-    self._left = None
-    self._right = None
+    def __init__(self, value):
+        self._root = None
+        self._value = value
+        self._left = None
+        self._right = None
 
-  # TODO: Implement node value insertion method
-  def insert_value(self, value, current_node=False):
-    if current_node == False:
-      node = TreeNode(value)
-      if self._root == None:
-        self._root = node
-        return self._root
-      else:
-        current_node = self._root
+    # TODO: Implement node value insertion iteratively
+    def insert_value(self, value, current_node=False):
+        if current_node == False:
+            node = TreeNode(value)
+            if self._root == None:
+                self._root = node
+                return self._root        
+            current_node = self._root
         if (value < current_node._value) and (current_node._left != None):
-          current_node = current_node._left
+            #traversing with pointer
+            current_node = current_node._left
         if (value >= current_node._value) and (current_node._right != None):
-          current_node = current_node._right
+            current_node = current_node._right
         if (current_node._left == None) and (value < current_node._value):
-          current_node._left = node
+            current_node._left = node
         if (current_node._right == None) and (value >= current_node._value):
-          current_node._right = node
+            current_node._right = node
+
+    # recursive insertion of BST
+    def insert_recursive(self, val, root=False):
+        # if the tree is currently empty, then create the node as the 'absolute' root
+        if root==False:
+            if self._root==None: 
+                self._root = TreeNode(val)
+                return 
+            root = self._root
+        # otherwise, the tree is not empty, so...
+        # if our value to insert is less than the root...
+        if val < root._value:
+            if root._left==None:                # ...and the left child does not exist,
+                root._left = TreeNode(val)      # then create the node as the left child
+            else:                               # ...and the left child already exists,
+                self.insert_recursive(val, root._left)    #then recursively insert on the left subtree
+        # if our value to insert is greater than or equal to the root...
+        else:
+            if root._right==None:               #  ...and the right child does not exist,
+                root._right = TreeNode(val)     #      then create the node as the right child
+            else:                               #  ...and the right child already exists,
+                self.insert_recursive(val, root._right);   #then recursively insert on the right subtree
 
 
-
-  # TODO: Implement iterative search method
-  def search_iteratively(self, value):
-    if self._root == None:
-      return False
-    current = self._root
-    while current != None:
-      if current._value == value:
-        return True
-      if current._value < value:
-        current = current._right
-      if current._value > value:
-        current = current._left
-    return False
-
-
-  # TODO: Implement recursive search method
-  def search_recursively(self, value, current_node=False):
-    if self._root == None:
-      return False
-    if current_node == False:
-      current_node = self._root
-    if value < current_node._value:
-      if current_node._left != None :
-        current_node = current_node._left
-      else:
+    # TODO: Implement iterative search method
+    def search_iteratively(self, value):
+        if self._root == None:
+            return False
+        current = self._root
+        while current != None:
+            if current._value == value:
+                return True
+            if current._value < value:
+                #traversal using moving pointer
+                current = current._right
+            if current._value > value:
+                current = current._left
         return False
-    elif value > self._root._value:
-      if current_node._right != None:
-        current_node = current_node._right
-      else:
-        return False
-    else:
-      return True
-    return self.search_recursively(value, current_node)
+
+
+    # TODO: Implement recursive search method
+    def search_recursively(self, value, current_node=False):
+        if self._root == None:
+            return False
+        if current_node == False:
+            current_node = self._root
+        if value < current_node._value:
+            if current_node._left != None :
+                current_node = current_node._left
+            else:
+                return False
+        elif value > self._root._value:
+            if current_node._right != None:
+                current_node = current_node._right
+            else:
+                return False
+        else:
+            return True
+        return self.search_recursively(value, current_node)
 
 
 
 
-# tree = BinarySearchTree(3)
-# print(tree._root)                         # None
+tree = BinarySearchTree(3)
+print(tree._root)                         # None
 
 # 1. Test node value insertion
-# tree.insert_value(10)
-# tree.insert_value(5)
-# tree.insert_value(16)
-# tree.insert_value(1)
-# tree.insert_value(7)
-# tree.insert_value(16)
-# print(tree._root._value)                  # 10
-# print(tree._root._left._value)            # 5
-# print(tree._root._right._value)           # 16
-# print(tree._root._left._left._value)      # 1
-# print(tree._root._left._right._value)     # 7
-# print(tree._root._right._right._value)    # 16
+tree.insert_value(10)
+tree.insert_value(5)
+tree.insert_value(16)
+tree.insert_value(1)
+tree.insert_value(7)
+tree.insert_value(16)
+# tree.insert_recursive(10)
+# tree.insert_recursive(5)
+# tree.insert_recursive(16)
+# tree.insert_recursive(1)
+# tree.insert_recursive(7)
+# tree.insert_recursive(16)
+print(tree._root._value)                  # 10
+print(tree._root._left._value)            # 5
+print(tree._root._right._value)           # 16
+print(tree._root._left._left._value)      # 1
+print(tree._root._left._right._value)     # 7
+print(tree._root._right._right._value)    # 16
 
-# # 2. Test iterative search
-# empty_tree = BinarySearchTree(2)
-# print(empty_tree.search_iteratively(10))  # False
-# print(tree.search_iteratively(10))        # True
-# print(tree.search_iteratively(7))         # True
-# print(tree.search_iteratively(-1))        # False
+# 2. Test iterative search
+empty_tree = BinarySearchTree(2)
+print(empty_tree.search_iteratively(10))  # False
+print(tree.search_iteratively(10))        # True
+print(tree.search_iteratively(7))         # True
+print(tree.search_iteratively(-1))        # False
 
 # 3. Test recursive search
-# print(empty_tree.search_recursively(10))  # False
-# print(tree.search_recursively(10))        # True
-# print(tree.search_recursively(7))         # True
-# print(tree.search_recursively(-1))        # False
+print(empty_tree.search_recursively(10))  # False
+print(tree.search_recursively(10))        # True
+print(tree.search_recursively(7))         # True
+print(tree.search_recursively(-1))        # False
